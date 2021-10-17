20a839e7be5c407f869e1ee3dbc70cb53a85a21b
horizontti <77754465+horizontti@users.noreply.github.com>
2021-10-17 22:53:25 +0300

    Update C.lua

--- Converter/C.lua
+++ Converter/C.lua
@@ -1,4 +1,3 @@
--- ESX.
 ESX.GetPlayerData                       = QBCore.Functions.GetPlayerData
 ESX.SetPlayerData                       = QBCore:Player:SetPlayerData
 ESX.TriggerServerCallback               = QBCore.Functions.TriggerCallback
@@ -20,41 +19,6 @@ ESX.SavePlayer                          = QBCore.Player.Save
 ESX.Trace                               = QBCore.Debug
 ESX.UseItem                             = QBCore.Functions.UseItem
 
--- xPlayer
-xPlayer.removeWeaponComponent           = xPlayer.Functions.RemoveItem (component name)
-xPlayer.setAccountMoney                 = xPlayer.Functions.SetMoney (account)
-xPlayer.setInventoryItem                = xPlayer.Functions.AddItem (item name)
-xPlayer.setJob                          = xPlayer.Functions.SetJob
-xPlayer.setMoney                        = xPlayer.Functions.SetMoney
-xPlayer.showHelpNotification            = TriggerClientEvent('QBCore:Notify')
-xPlayer.showNotification                = TriggerClientEvent('QBCore:Notify')
-xPlayer.setCoords                       = None (used for teleporting)
-xPlayer.setMaxWeight                    = None (It is set in qb-core config)
-xPlayer.setName                         = None (dont bother)
-xPlayer.setWeaponTint                   = None (qb-weapons does this)
-xPlayer.triggerEvent                    = None (dont bother)
-xPlayer.updateCoords                    = None (dont bother)
-
--- esx:
-esx:onPlayerDeath                       = hospital:server:SetDeathStatus
-esx:playerLoaded                        = QBCore:Client:OnPlayerLoaded (use for setting a variable to let the script know the player is ready)
-esx:showAdvancedNotification            = QBCore:Notify
-esx:showHelpNotification                = QBCore:Notify
-esx:showNotification                    = QBCore:Notify
-esx:getSharedObject                     = QBCore:GetObject
-esx:setJob                              = QBCore:Client:OnJobUpdate
-esx:onPlayerSpawn                       = QBCore:Client:OnPlayerLoaded
-playerSpawned                           = QBCore:Client:OnPlayerLoaded (spawnmanager compatibility)
-esx:addInventoryItem                    = QBCore:Server:AddItem
-esx:removeInventoryItem                 = QBCore:Server:RemoveItem
-esx:useItem                             = QBCore:Server:UseItem
-
--- SQL
-MySQL.Async.fetchScalar()               = exports['ghmattimysql']:scalar() or QBCore.Functions.ExecuteSql(true,
-MySQL.Async.fetchAll()                  = exports['ghmattimysql']:execute() or QBCore.Functions.ExecuteSql(true,
-MySQL.Async.execute()                   = exports['ghmattimysql']:execute() or QBCore.Functions.ExecuteSql(false,
-
--- None
 ESX.IsPlayerLoaded                      = None (checks if player is loaded so not relevant)
 ESX.Game.DeleteObject                   = None (Can use FiveM native DeleteEntity)
 ESX.Game.GetClosestObject               = None (Can use FiveM native GetClosestObjectOfType)
@@ -71,6 +35,35 @@ ESX.Game.SpawnObject                    = None (Can use FiveM Native CreateObjec
 ESX.CreatePickup                        = None (irrelevant and done through qb-inventory)
 ESX.GetItemLabel                        = None (Just returns item label)
 ESX.SavePlayers                         = None (dont bother)
-
--- Other
 ESX.Game.Teleport                       = (Can use FiveM Native SetEntityCoords and SetEntityHeading)
+  
+esx:onPlayerDeath                       = hospital:server:SetDeathStatus
+esx:playerLoaded                        = QBCore:Client:OnPlayerLoaded (use for setting a variable to let the script know the player is ready)
+esx:showAdvancedNotification            = QBCore:Notify
+esx:showHelpNotification                = QBCore:Notify
+esx:showNotification                    = QBCore:Notify
+esx:getSharedObject                     = QBCore:GetObject
+esx:setJob                              = QBCore:Client:OnJobUpdate
+esx:onPlayerSpawn                       = QBCore:Client:OnPlayerLoaded
+playerSpawned                           = QBCore:Client:OnPlayerLoaded (spawnmanager compatibility)
+esx:addInventoryItem                    = QBCore:Server:AddItem
+esx:removeInventoryItem                 = QBCore:Server:RemoveItem
+esx:useItem                             = QBCore:Server:UseItem
+
+xPlayer.removeWeaponComponent           = xPlayer.Functions.RemoveItem (component name)
+xPlayer.setAccountMoney                 = xPlayer.Functions.SetMoney (account)
+xPlayer.setInventoryItem                = xPlayer.Functions.AddItem (item name)
+xPlayer.setJob                          = xPlayer.Functions.SetJob
+xPlayer.setMoney                        = xPlayer.Functions.SetMoney
+xPlayer.showHelpNotification            = TriggerClientEvent('QBCore:Notify')
+xPlayer.showNotification                = TriggerClientEvent('QBCore:Notify')
+xPlayer.setCoords                       = None (used for teleporting)
+xPlayer.setMaxWeight                    = None (It is set in qb-core config)
+xPlayer.setName                         = None (dont bother)
+xPlayer.setWeaponTint                   = None (qb-weapons does this)
+xPlayer.triggerEvent                    = None (dont bother)
+xPlayer.updateCoords                    = None (dont bother)
+  
+MySQL.Async.fetchScalar()               = exports['ghmattimysql']:scalar() or QBCore.Functions.ExecuteSql(true,
+MySQL.Async.fetchAll()                  = exports['ghmattimysql']:execute() or QBCore.Functions.ExecuteSql(true,
+MySQL.Async.execute()                   = exports['ghmattimysql']:execute() or QBCore.Functions.ExecuteSql(false,
